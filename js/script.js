62c4b12c3bedb22dec9d0662c655ec967c4a01dd
Ekaterina Ganshina <ekaterina.ganshina@bk.ru>
2018-02-18 22:01:55 +0300

    Minor fixes

--- js/script.js
+++ js/script.js
@@ -1,33 +1,35 @@
-const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-const date = new Date();
+var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+var date = new Date();
 
 // Preload background image
-$(window).on('load', () => { $('.page-header .background').css('opacity', '1') });
+$(window).on('load', function() {
+    $('.page-header .background').css('opacity', '1')
+});
 
 $(document).ready(function () {
-    const $header = $('.page-header');
-    const $services = $('.services');
-    const $today = $('.today-post .date');
+    var $header = $('.page-header');
+    var $services = $('.services');
+    var $today = $('.today-post .date');
     
     // Set date for "Today Post" section
     $today.find('.month').text(months[date.getMonth()]);
     $today.find('.day').text(date.getDate());
     
     // Scroll to top
-    $('.page-footer .go-top').on('click', () => {
+    $('.page-footer .go-top').on('click', function () {
         $('html, body').animate({
             scrollTop: 0
         }, 500)
     });
     
     // Switch the articles (section "Our Services")
-    $services.find('.services-list li').on('click', (e) => {
-        const article = $(e.target).data('article');
-        const $visible = $services.find('.articles-list .article-wrapper:visible');
+    $services.find('.services-list li').on('click', function(e) {
+        var article = $(e.target).data('article');
+        var $visible = $services.find('.articles-list .article-wrapper:visible');
         
         $services.find('.services-list li.active').removeClass('active');
         $(e.target).addClass('active');
-        $visible.fadeOut(300, () => {
+        $visible.fadeOut(300, function() {
             $services.find('.articles-list [data-title="' + article + '"]').fadeIn(300);
         });
     });
@@ -42,11 +44,13 @@ $(document).ready(function () {
     // Close header nav
     $header.find('.main-nav-mobile .close-menu').on('click', closeHeaderNav);
     
+    // Toggle footer nav
+    $('.page-footer .menu').on('click', function(e) {
+        $(e.currentTarget).next().toggleClass('shown')
+    });
+    
     function closeHeaderNav() {
         $header.removeClass('nav-opened');
         $(document).off('click touchmove', closeHeaderNav);
     }
-    
-    // Toggle footer nav
-    $('.page-footer .menu').on('click', (e) => { $(e.currentTarget).next().toggleClass('shown') });
 });
