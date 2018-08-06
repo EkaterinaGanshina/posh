1f76729d3577102890abb90364fed7cdb6594047
Ekaterina Ganshina <ekaterina.ganshina@bk.ru>
2018-08-06 23:33:43 +0300

    Add carousel to header section

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Posh Project
-Main page of the celebration agency site.
+Main page of style & beauty site.
 
 This is the demo project for my portfolio, you can see it in action at http://ganshina.com/posh/ 
 
 Author: Ekaterina Ganshina ekaterina@ganshina.com
 
-PSD layout is provided for free by [HTML Academy](https://htmlacademy.ru). Thank you! :heart:
+PSD layout is provided for free by [Juno Team](https://junoteam.com/portfolio/posh-free-psd-beauty-template/). Thank you!
--- js/script.js
+++ js/script.js
@@ -1,56 +1,66 @@
+"use strict";
+
 var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 var date = new Date();
 
 // Preload background image
-$(window).on('load', function() {
-    $('.page-header .background').css('opacity', '1')
+$(window).on('load', function () {
+  $('.page-header .header-background').css('opacity', '1')
 });
 
 $(document).ready(function () {
-    var $header = $('.page-header');
-    var $services = $('.services');
-    var $today = $('.today-post .date');
-    
-    // Set date for "Today Post" section
-    $today.find('.month').text(months[date.getMonth()]);
-    $today.find('.day').text(date.getDate());
-    
-    // Scroll to top
-    $('.page-footer .go-top').on('click', function () {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 500)
+  var $header = $('.page-header');
+  var $services = $('.services');
+  var $today = $('.today-post .date');
+
+  // Initialize carousel in the header
+  $('.header-background').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 5000
+  });
+
+  // Set date for "Today Post" section
+  $today.find('.month').text(months[date.getMonth()]);
+  $today.find('.day').text(date.getDate());
+
+  // Scroll to top
+  $('.page-footer .go-top').on('click', function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 500)
+  });
+
+  // Switch the articles (section "Our Services")
+  $services.find('.services-list li').on('click', function (e) {
+    var article = $(e.target).data('article');
+    var $visible = $services.find('.articles-list .article-wrapper:visible');
+
+    $services.find('.services-list li.active').removeClass('active');
+    $(e.target).addClass('active');
+    $visible.fadeOut(300, function () {
+      $services.find('.articles-list [data-title="' + article + '"]').fadeIn(300);
     });
-    
-    // Switch the articles (section "Our Services")
-    $services.find('.services-list li').on('click', function(e) {
-        var article = $(e.target).data('article');
-        var $visible = $services.find('.articles-list .article-wrapper:visible');
-        
-        $services.find('.services-list li.active').removeClass('active');
-        $(e.target).addClass('active');
-        $visible.fadeOut(300, function() {
-            $services.find('.articles-list [data-title="' + article + '"]').fadeIn(300);
-        });
-    });
-    
-    // Open header nav
-    $header.find('.open-menu').on('click', function (e) {
-        $header.addClass('nav-opened');
-        e.stopPropagation();
-        $(document).on('click touchmove', closeHeaderNav);
-    });
-    
-    // Close header nav
-    $header.find('.main-nav-mobile .close-menu').on('click', closeHeaderNav);
-    
-    // Toggle footer nav
-    $('.page-footer .menu').on('click', function(e) {
-        $(e.currentTarget).next().toggleClass('shown')
-    });
-    
-    function closeHeaderNav() {
-        $header.removeClass('nav-opened');
-        $(document).off('click touchmove', closeHeaderNav);
-    }
+  });
+
+  // Open header nav
+  $header.find('.open-menu').on('click', function (e) {
+    $header.addClass('nav-opened');
+    e.stopPropagation();
+    $(document).on('click touchmove', closeHeaderNav);
+  });
+
+  // Close header nav
+  $header.find('.main-nav-mobile .close-menu').on('click', closeHeaderNav);
+
+  // Toggle footer nav
+  $('.page-footer .menu').on('click', function (e) {
+    $(e.currentTarget).next().toggleClass('shown')
+  });
+
+  function closeHeaderNav() {
+    $header.removeClass('nav-opened');
+    $(document).off('click touchmove', closeHeaderNav);
+  }
 });
