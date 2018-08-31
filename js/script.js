8f0a3fbc7d231e3c57e2d170ec0ab046906c4d9d
Ekaterina Ganshina <ekaterina.ganshina@bk.ru>
2018-08-31 23:37:33 +0300

    Add styles to adapt slick carousel

--- js/script.js
+++ js/script.js
@@ -18,7 +18,8 @@ $(document).ready(function () {
     arrows: false,
     dots: true,
     autoplay: true,
-    autoplaySpeed: 5000
+    autoplaySpeed: 5000,
+    fade: true
   });
 
   // Set date for "Today Post" section
